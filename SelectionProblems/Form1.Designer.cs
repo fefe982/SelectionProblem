d1ebbbb3b6e410874d1dfe66b8d4cf98126eecb0
Yongxin Wang <fefe.wyx@gmail.com>
2015-05-26 11:27:02 +0800

    Selection problem tester, Init

---
+++ SelectionProblems/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace SelectionProblems
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textQuestion = new System.Windows.Forms.RichTextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.button_Next = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textQuestion
+            // 
+            this.textQuestion.Location = new System.Drawing.Point(12, 12);
+            this.textQuestion.Name = "textQuestion";
+            this.textQuestion.ReadOnly = true;
+            this.textQuestion.Size = new System.Drawing.Size(858, 244);
+            this.textQuestion.TabIndex = 0;
+            this.textQuestion.TabStop = false;
+            this.textQuestion.Text = "请点击下一题";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 274);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(29, 16);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "A";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(113, 274);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(29, 16);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "B";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(214, 274);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(29, 16);
+            this.radioButton3.TabIndex = 3;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "C";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(315, 274);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(29, 16);
+            this.radioButton4.TabIndex = 4;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "D";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // button_Next
+            // 
+            this.button_Next.Location = new System.Drawing.Point(402, 267);
+            this.button_Next.Name = "button_Next";
+            this.button_Next.Size = new System.Drawing.Size(75, 23);
+            this.button_Next.TabIndex = 5;
+            this.button_Next.Text = "下一题";
+            this.button_Next.UseVisualStyleBackColor = true;
+            this.button_Next.Click += new System.EventHandler(this.button_Next_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(882, 314);
+            this.Controls.Add(this.button_Next);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.textQuestion);
+            this.Name = "MainForm";
+            this.Text = "测试";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox textQuestion;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.Button button_Next;
+    }
+}
+
---
+++ SelectionProblems/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SelectionProblems
+{
+    public partial class MainForm : Form
+    {
+        int qIndex = 0;
+        int totalNum = 0;
+        int scorePerQ = 0;
+        string answer;
+        int totalCorrect = 0;
+        int selected = -1;
+        public MainForm()
+        {
+            InitializeComponent();
+
+            char []whitespace = new char[]{' ', '\t', '\r', '\n'};
+            StreamReader fs = new StreamReader(new FileStream("data/answer.txt", FileMode.Open, FileAccess.Read));
+            String line = fs.ReadLine();
+            line.Trim();
+            int idx = line.IndexOfAny(whitespace);
+            if (idx > 0)
+            {
+                line = line.Remove(idx);
+            }
+            scorePerQ = Int32.Parse(line);
+
+            line = fs.ReadLine();
+            line.Trim();
+            idx = line.IndexOfAny(whitespace);
+            if (idx > 0)
+            {
+                line = line.Remove(idx);
+            }
+            totalNum = Int32.Parse(line);
+
+            line = fs.ReadLine();
+            line.Trim();
+            line = line.ToUpper();
+            for (int i = 0; i < line.Length; i++)
+            {
+                if (line[i] < 'A' || line[i] > 'D')
+                {
+                    line = line.Remove(i, 1);
+                    i--;
+                }
+            }
+
+            if (line.Length != totalNum)
+            {
+                MessageBox.Show("总题数与答案个数不一致！");
+            }
+            answer = line;
+        }
+
+        private void button_Next_Click(object sender, EventArgs e)
+        {
+            if (qIndex > 0 && qIndex <= totalNum)
+            {
+                selected = -1;
+                if (radioButton1.Checked)
+                {
+                    selected = 0;
+                }
+                else if (radioButton2.Checked)
+                {
+                    selected = 1;
+                }
+                else if (radioButton3.Checked)
+                {
+                    selected = 2;
+                }
+                else if (radioButton4.Checked)
+                {
+                    selected = 3;
+                }
+                if (selected == -1)
+                {
+                    MessageBox.Show("请选择一个选项");
+                    return;
+                }
+                else
+                {
+                    if (selected == answer[qIndex - 1] - 'A')
+                    {
+                        totalCorrect++;
+                    }
+                }
+            }
+            qIndex++;
+            if (qIndex <= totalNum)
+            {
+                textQuestion.LoadFile(String.Format("data/{0:0000}.rtf", qIndex));
+            }
+            else
+            {
+                String msg = String.Format("测试结束，正确{0}题，总分{1}分", totalCorrect, totalCorrect * scorePerQ);
+                textQuestion.Text = msg;
+                MessageBox.Show(msg);
+                radioButton1.Enabled = false;
+                radioButton2.Enabled = false;
+                radioButton3.Enabled = false;
+                radioButton4.Enabled = false;
+            }
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
+            radioButton3.Checked = false;
+            radioButton4.Checked = false;
+        }
+    }
+}
